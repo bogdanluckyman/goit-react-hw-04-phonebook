99b3fde0905cd23afe0262d4fed2aca70373ca9f
Bogdan <bogdantisenko06@gmail.com>
2023-11-28 00:49:50 +0200

    finish

--- src/components/App.js
+++ src/components/App.js
@@ -5,27 +5,18 @@ import { ContactList } from './ContactList';
 import { nanoid } from 'nanoid';
 import { SearchBar } from './SearchBar';
 
-export const App = () => {
-  // const contactsArr = [
-  //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  // ];
+const initialContacts = () => {
+  const savedContacts = localStorage.getItem('newContact');
+  return savedContacts !== null ? JSON.parse(savedContacts) : [];
+};
 
-  const [contacts, setContacts] = useState([]);
+export const App = () => {
+  const [contacts, setContacts] = useState(initialContacts());
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
     localStorage.setItem('newContact', JSON.stringify(contacts));
   }, [contacts]);
-
-  useEffect(() => {
-    const savedContacts = localStorage.getItem('newContact');
-    if (savedContacts) {
-      setContacts(JSON.parse(savedContacts));
-    }
-  }, []);
 
   const addContact = newContact => {
     const { name, number } = newContact;
